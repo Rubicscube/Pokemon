b8a57a516990d40f78a5b1308d9b1fe5e001bb47
RobinMarien <dev.robinmarien@gmail.com>
2019-09-12 16:58:40 +0200

    end of the day

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const api_url = "https://pokeapi.co/api/v2/pokemon/";
+const moveAmount = 4;
 
-
-
+//SEARCH BUTTON
 document.getElementById("run").addEventListener("click", function(){
 
     let input = document.getElementById('pokemon').value.toLowerCase();
-
     fetcher(input);
 
 });
@@ -15,13 +14,34 @@ function fetcher(input){
         .then(function(response){
             return response.json();
         })
-        .then(function(data) {
+        .then(function(pokemon) {
+            //ALL THE INFO
+            console.log(pokemon);
 
-            console.log(data);
             //NAME
-            console.log(data.name);
+            console.log(pokemon.name);
+            let name = document.createElement('p');
+
+            //ID
+            console.log(pokemon.id);
 
             //IMAGE
-            console.log(data.sprites.front_default)
+            console.log(pokemon.sprites.front_default);
+
+            //DISPLAY 4 RANDOM MOVES (or less, if the pokemon doesn't have 4 moves)
+            if (pokemon.moves.length < moveAmount){
+
+                for (let i = 0; i < pokemon.moves.length; i++) {
+                    console.log(pokemon.moves[i].move.name);
+                }
+            }
+            else{
+                for (let i = 0; i < moveAmount; i++) {
+                    let list = Math.floor(Math.random() * pokemon.moves.length);
+                    console.log(pokemon.moves[list].move.name);
+                }
+            }
         });
 }
+
+//document.getElementById("prev")
