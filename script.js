ce0679b6d8cbbe12b4339fe1aad72171b3ca4555
RobinMarien <dev.robinmarien@gmail.com>
2019-09-12 14:10:17 +0200

    wrote fetch function

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const api_url = "https://pokeapi.co/api/v2/pokemon/";
-let input = document.getElementById('pokemon').value;
 
 
 
-fetch(api_url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+document.getElementById("run").addEventListener("click", function(){
 
+    let input = document.getElementById('pokemon').value;
+
+    fetcher(input);
+    
+});
+
+function fetcher(input){
+    fetch(api_url+input)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+}
